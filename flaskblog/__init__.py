318a849e7563fd572a497bb7c78f6d365c36d701
emericen <liang.741@buckeyemail.osu.edu>
2021-01-26 16:01:18 +0800

    first commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0f88ed581f6913b05571b661b7c9f1e2'
+
+from flaskblog import routes
+
---
+++ run.py
@@ -0,0 +1,24 @@
+from flaskblog import app
+
+
+if __name__=='__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
